56cce77a0ede2ac38d984c30af9bea4253d2bddf
Martin Thuresson <martin.thure@gmail.com>
2019-01-03 12:57:50 +0100

    update to stor to db

--- relay.py
+++ relay.py
@@ -4,13 +4,17 @@
 from gevent import monkey; monkey.patch_all()
 from gevent import spawn, sleep, pywsgi
 from geventwebsocket.handler import WebSocketHandler
+from pymongo import MongoClient
 import uuid
 import websocket
+from datetime import datetime
+
 
 #
 # Global data to be accessed from multipple requests
 #
 listeners = {}
+db = MongoClient().trainpos
 
 # Listen for WebSocket and attach WebSocket to the listeners array.
 #
@@ -36,8 +40,31 @@ def listenforevents(environ, start_response):
             sleep(0.25)
     del listeners[key]
 
+def readnmea(data):
+    direction = {"N":1,"S":-1,"W":-1,"E":1}
+    out = {}
+    parts = data.split(',')
+    out["time"] = datetime_object = datetime.strptime(parts[9] + "T" + parts[1].split('.')[0], '%d%m%yT%H%M%S') 
+    out["location"] = {
+      "type": "Point",
+      "coordinates": [
+        float(parts[5][0:3]) + float(parts[5][3:])/60.0 * direction[parts[6]],
+        float(parts[3][0:2]) + float(parts[3][2:])/60.0 * direction[parts[4]]]
+    }
+    out["speed"] = float(parts[7]) * 1.85200
+    out["id"] = parts[14]
+    try:
+        out["direction"] = float(parts[8])
+    except:
+        out["direction"] = None
+    return out
+
+#$GPRMC,114632.0,A,6000.966781,N,01308.263916,E,4.7,170.9,030119,0.0,E,A*02,,1414.trains.se,,8964.public.trains.se@2019-01-03;8964.internal.trains.se@2019-01-03,oxyfi
 def sendtoall(data):
     global listeners
+    global db
+    structdata = readnmea(data)
+    db.postdata.insert(structdata)
     for one in listeners:
         listeners[one].append(data)
 
@@ -45,7 +72,7 @@ class nologgerclass(object):
   def __init__(self):
     self.current = ''
   def write(self, data):
-      pass
+    pass
 
 nologger = nologgerclass()
 
@@ -66,6 +93,7 @@ def on_open(ws):
     print "### Open ###"
 
 websocket.enableTrace(True)
+#ws = websocket.WebSocketApp("wss://map.thure.org/oxyfigps/",
 ws = websocket.WebSocketApp("wss://api.oxyfi.com/trainpos/listen?v=1&key=e6ab725a30ef45f6a38c3a9b83f5f65a",
                               on_message = on_message,
                               on_error = on_error,
